25321991e58f300ea392fa3dbf33a3341f1c275e
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-03-16 15:26:10 +0200

    Update ProductDetail layout

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ProductCard({ product, onSelectProduct }) {
   return (
     <div onClick={() => onSelectProduct(product)}>
-      <div >
+      <div className='cursor-pointer'>
           <img src={product.image} alt={product.name} className='h-50 w-50 object-cover bg-white rounded-xl mb-4 p-4' />
           <h3>{product.name}</h3>
           <h4 className='text-gray-400'>{product.model}</h4>
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -13,24 +13,29 @@ function ProductDetail({ product, onBack }) {
       <div onClick={onBack} className='flex items-center gap-4 cursor-pointer'>
         <IoIosArrowBack /> back
       </div>
-      <div>
-        <img src={product.image} alt={product.name} className='object-contain h-80' />
+
+      <div className='flex gap-8 mt-6'>
+        <img src={product.image} alt={product.name} className='object-contain h-60 bg-white rounded-xl' />
 
         <div>
-          <h1>{product.name}</h1>
-          <h3>{product.model}</h3>
-          <h4>{product.price}</h4>
-          <p>{product.shortDescription}</p>
+          <div>
+            <h1 className='text-5xl'>{product.name}</h1>
+            <h3 className='text-2xl'>{product.model}</h3>
+            <h4 className='text-2xl'>{product.price}</h4>
+            <p>{product.shortDescription}</p>
+          </div>
+
+          <button className='flex rounded bg-black text-white p-2 items-center gap-2'>
+            <BsBagPlusFill /> Add to bag
+          </button>
         </div>
 
       </div>
 
-      <button>
-        <BsBagPlusFill /> Add to bag
-      </button>
+      <hr className='border-gray-300 border-t-4 my-8'/>
 
-      <hr />
-      <div>
+      <div className=''>
+        <h1 className='text-4xl mb-4'>Description</h1>
         <p>{product.description}</p>
       </div>
     </div>
